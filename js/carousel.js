d6898c41c7cfe2b58c5913201bbf8f81c609ec31
Bozhidar Yordanov <108609381+BozhidarYordanov54@users.noreply.github.com>
2024-04-30 13:47:14 +0300

    Reworke the real estate carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,42 +1,36 @@
-let touchStartX = 0;
-let touchEndX = 0;
-
-function handleGesture() {
-    if (touchEndX < touchStartX) {
-        navigateSlide("next");
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    slidesPerGroup: 2,
+    loop: true,
+    centeredSlides: true,
+    grabCursor: "true",
+    fade: "true",
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            slidesPerGroup: 1,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+            slidesPerGroup: 2,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+            slidesPerGroup: 1,
+        }
     }
-    if (touchEndX > touchStartX) {
-        navigateSlide("prev");
-    }
-}
-
-function navigateSlide(direction) {
-    const offset = direction === "next" ? 1 : -1;
-    const carousel = document.querySelector('[data-carousel]');
-    const slides = carousel.querySelector('[data-carousel-slide]');
-    const activeSlide = slides.querySelector('[data-active]');
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-    if(newIndex < 0) newIndex = slides.children.length - 1;
-    if(newIndex >= slides.children.length) newIndex = 0;
-
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
-}
-
-const buttons = document.querySelectorAll('[data-carousel-button]');
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const direction = button.dataset.carouselButton;
-        navigateSlide(direction);
-    });
-});
-
-const carousel = document.querySelector('[data-carousel]');
-carousel.addEventListener('touchstart', e => {
-    touchStartX = e.changedTouches[0].screenX;
-});
-
-carousel.addEventListener('touchend', e => {
-    touchEndX = e.changedTouches[0].screenX;
-    handleGesture();
-});
+})
