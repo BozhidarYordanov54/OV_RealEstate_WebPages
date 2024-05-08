0270d7236e1f6f09c8ca1b71a5bde864fec89566
Nikola <nikola88393@gmail.com>
2024-05-08 12:39:04 +0300

    Import listing carousel and style floating label

--- js/carousel.js
+++ js/carousel.js
@@ -1,58 +1,76 @@
 var swiper = new Swiper(".slide-container", {
-    slidesPerView: 2,
-    spaceBetween: 10,
-    slidesPerGroup: 2,
-    loop: true,
-    centeredSlides: true,
-    grabCursor: "true",
-    fade: "true",
-    pagination: {
-        el: ".swiper-pagination.estates",
-        clickable: true,
-        dynamicBullets: true,
+  slidesPerView: 2,
+  spaceBetween: 10,
+  slidesPerGroup: 2,
+  loop: true,
+  centeredSlides: true,
+  grabCursor: "true",
+  fade: "true",
+  pagination: {
+    el: ".swiper-pagination.estates",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next.swiper-navBtn",
+    prevEl: ".swiper-button-prev.swiper-navBtn",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      slidesPerGroup: 1,
     },
-    navigation: {
-        nextEl: ".swiper-button-next.swiper-navBtn",
-        prevEl: ".swiper-button-prev.swiper-navBtn",
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+      slidesPerGroup: 2,
     },
-    
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            slidesPerGroup: 1,
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-            slidesPerGroup: 2,
-        },
-        1024: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-            slidesPerGroup: 1,
-        }
-    }
-})
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+      slidesPerGroup: 1,
+    },
+  },
+});
 
 var swiper2 = new Swiper(".article-swiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    slidesPerGroup: 1,
-    loop: true,
-    centeredSlides: true,
-    grabCursor: "true",
-    fade: "true",
-    pagination: {
-        el: ".swiper-pagination.article",
-        clickable: true,
-        dynamicBullets: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next.article-btn",
-        prevEl: ".swiper-button-prev.article-btn",
-    },
-    autoplay: {
-        delay: 3000,
-    },
-})
+  slidesPerView: 1,
+  spaceBetween: 10,
+  slidesPerGroup: 1,
+  loop: true,
+  centeredSlides: true,
+  grabCursor: "true",
+  fade: "true",
+  pagination: {
+    el: ".swiper-pagination.article",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next.article-btn",
+    prevEl: ".swiper-button-prev.article-btn",
+  },
+  autoplay: {
+    delay: 3000,
+  },
+});
+
+var swiper3 = new Swiper(".swiper-listing", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
