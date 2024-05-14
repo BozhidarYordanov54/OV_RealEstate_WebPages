15b2747b26b78c1b9e2ac70564b2be44d3830e9d
BozhidarYordanov54 <bozhidar.yordanov02@gmail.com>
2024-05-14 19:19:26 +0300

    Implemented select2 library for filters

--- js/selectSearch.js
+++ js/selectSearch.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
-    $('.estate-type').select2({
+    $('.town').select2({
+        placeholder: 'Град',
     });
 });
+
+$().ready(function() {
+    $('.neighborhood').select2({
+        placeholder: 'Квартал',
+    });
+});
+
+$(document).ready(function() {
+    $('.estate-type').select2({
+        placeholder: 'Тип имот',
+    });
+});
+
