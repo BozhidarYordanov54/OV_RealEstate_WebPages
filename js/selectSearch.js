38db12fb873c1a2007080722988b90e45be2067b
BozhidarYordanov54 <bozhidar.yordanov02@gmail.com>
2024-05-14 20:15:40 +0300

    Made the select2 inputs react responsively to the width/height of device

--- js/selectSearch.js
+++ js/selectSearch.js
@@ -1,36 +1,42 @@
 $(document).ready(function() {
     $('.town').select2({
         placeholder: 'Град',
+        width: '100%',
     });
 });
 
 $().ready(function() {
     $('.neighborhood').select2({
         placeholder: 'Квартал',
+        width: '100%',
     });
 });
 
 $(document).ready(function() {
     $('.estate-type').select2({
         placeholder: 'Тип имот',
+        width: '100%',
     });
 });
 
 $(document).ready(function() {
     $('.construction-type').select2({
         placeholder: 'Тип строителство',
+        width: '100%',
     });
 });
 
 $(document).ready(function() {
     $('.furnished').select2({
         placeholder: 'Обзавеждане',
+        width: '100%',
     });
 });
 
 $(document).ready(function() {
     $('.heating').select2({
         placeholder: 'Отопление',
+        width: '100%',
     });
 });
 
