cfc12390809480f83d235e90dec40d40b0385a33
Nikola <nikola88393@gmail.com>
2024-05-20 13:27:25 +0300

    Rework and optimize scroll animation for blog posts

--- js/scrollAnimation.js
+++ js/scrollAnimation.js
@@ -1,26 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const observerOptions = {
-    threshold: Array.from({ length: 101 }, (_, i) => i / 100),
-  };
+const observerOptions = {
+  threshold: 0.1,
+};
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      const intersectionRatio = entry.intersectionRatio;
-      const minOpacity = 0.3;
-      const maxOpacity = 1;
-      // Calculate the opacity based on intersection ratio
-      const calculatedOpacity =
-        minOpacity + (maxOpacity - minOpacity) * intersectionRatio;
-      entry.target.style.opacity = calculatedOpacity;
-    });
-  }, observerOptions);
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+}, observerOptions);
 
-  const blogPostContainer = document.querySelectorAll(
-    ".blog-posts-container .blog-post"
-  );
-  if (blogPostContainer) {
-    blogPostContainer.forEach((blog) => {
-      observer.observe(blog);
-    });
-  }
-});
+const elements = document.querySelectorAll(".blog-post");
+elements.forEach((el) => observer.observe(el));
