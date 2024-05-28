b1d038c962569efc2221063f81e33a5290971aea
Bozhidar Yordanov <108609381+BozhidarYordanov54@users.noreply.github.com>
2024-05-28 15:54:09 +0300

    Changed burger button for nav

--- js/burger-button.js
+++ js/burger-button.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var burger = document.querySelector('.burger');
+    const hamburger = document.querySelector('.hamb');
     var menu = document.querySelector('.main-nav-wrapper');
     burger.addEventListener('click', function() {
         burger.classList.toggle('active');
+        hamburger.classList.toggle('active');
         menu.classList.toggle('active');
     });
 });
